0cfe067b747c060cef268dd5e0892ab7ba27d03a
branaji21 <20231926@nbsc.edu.ph>
2026-04-08 12:55:36 +0800

    added changes

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './ExploreContainer.css';
 
-interface ContainerProps { }
-
-const ExploreContainer: React.FC<ContainerProps> = () => {
+const ExploreContainer: React.FC = () => {
   return (
     <div id="container">
       <strong>Ready to create an app?</strong>
