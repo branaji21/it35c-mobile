df2e108706c5c3764f00545433522f9e2a1b6b12
branaji21 <20231926@nbsc.edu.ph>
2026-04-20 15:52:45 +0800

    adding feature for searching

--- src/pages/home-Tabs/Search.tsx
+++ src/pages/home-Tabs/Search.tsx
@@ -1,21 +1,20 @@
-import { IonButton, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle } from "@ionic/react";
+
 
 const Search: React.FC = () => {
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButton>
-                       <IonMenuButton></IonMenuButton>
-                       <IonTitle>Search</IonTitle>
-                    </IonButton >
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                <h1>Search</h1>
-            </IonContent>
-        </IonPage>
-    );
+  return (
+    <IonPage>
+        <IonHeader>
+            <IonButtons>
+                <IonMenuButton></IonMenuButton>
+                <IonTitle>Search</IonTitle>
+            </IonButtons>
+        </IonHeader>
+        <IonContent className="ion-padding">
+            <h1>Search</h1>
+        </IonContent>
+    </IonPage>
+  )
 };
 
 export default Search;
